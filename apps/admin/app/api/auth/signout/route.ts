939c70f0b9a3ae88348f6f1d38c1ef59857b54fb
Goddess <233002065+WhosOnPole@users.noreply.github.com>
2026-01-06 22:59:12 -0500

    Cookie Store issues

--- apps/admin/app/api/auth/signout/route.ts
+++ apps/admin/app/api/auth/signout/route.ts
@@ -3,8 +3,9 @@ import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function POST() {
+  const cookieStore = await cookies()
   const supabase = createRouteHandlerClient(
-    { cookies },
+    { cookies: () => cookieStore },
     {
       supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
       supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
