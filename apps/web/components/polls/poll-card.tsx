0ddbfe6739e37fe638305661d846ef0b85b63b28
Goddess <233002065+WhosOnPole@users.noreply.github.com>
2026-05-23 17:44:01 -0400

    Update poll-card.tsx

--- apps/web/components/polls/poll-card.tsx
+++ apps/web/components/polls/poll-card.tsx
@@ -124,7 +124,7 @@ export function PollCard({
     <div
       className={`h-full w-full rounded-lg p-0 shadow ${
         isDark
-          ? ' backdrop-blur-sm text-white'
+          ? ' backdrop-blur-sm bg-white/10 text-white'
           : 'border-gray-200 bg-white text-black'
       } ${className || ''}`}
     >
